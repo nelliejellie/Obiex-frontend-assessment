a0675b89c1b6b1717c75ce40c766a4e2d83e5e87
Emeka Ewelike <emekaewelike@gmail.com>
2023-04-13 15:43:20 +0100

    finish up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { BsSearch } from 'react-icons/bs';
 
 function Navbar() {
   return (
-    <section className='h-[80px] w-full bg-[#1D283C] flex flex-row justify-around items-center'>
+    <section className='h-[80px] w-full bg-[#1D283C] flex flex-row justify-around md:justify-between md:px-4 items-center'>
         <div className='flex flex-row items-center space-x-2'>
             <img 
                 src={require('../assets/images/logo.png')}
@@ -12,11 +12,11 @@ function Navbar() {
             <span className='text-white font-bold'>Vader</span>
         </div>
         <div>
-            <div className='border border-gray-600 rounded-md bg-[#1D283C] md:hidden flex flex-row items-center space-x-2 p-2 w-[331px]'>
+            <div className='border border-gray-600 rounded-md mt-2 bg-[#1D283C] md:hidden flex flex-row items-center space-x-2 p-2 w-[331px]'>
                 <BsSearch className='text-white'/>
-                <input type="text" placeholder='Search' className='outline-0 bg-[#1D283C] text-white'/>
+                <input type="text" placeholder='Search' className='outline-0 bg-[#1D283C] text-white w-[100%]'/>
             </div>
-            <BsSearch className='md:text-white md:w-[24px] md:h-[24px] hidden'/>
+            <BsSearch className='text-[#1D283C] md:text-white md:w-[24px] md:h-[24px]'/>
         </div>
     </section>
   )
